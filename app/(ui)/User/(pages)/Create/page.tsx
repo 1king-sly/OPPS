a201119ae27b440ba4267e775abfc3e1115608f4
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-01-24 13:24:08 +0300

    minor updates

--- app/(ui)/User/(pages)/Create/page.tsx
+++ app/(ui)/User/(pages)/Create/page.tsx
@@ -31,7 +31,7 @@ export default async  function Page() {
       <div className='w-full min-h-screen flex flex-col items-center justify-center pb-40'>
         <div className='flex flex-col  items-center'>
           <Image src={logo} alt='logo' className='lg:h-24 lg:w-24 object-cover h-16 w-16'></Image>
-          <h3 className='text-sky-300 max-[425px]:text-xs'>APPLIED SCIENCES PROJECT PROPOSAL</h3>
+          <h3 className='text-sky-300 max-[425px]:text-xs'>MMUST ONLINE PROJECT PROPOSAL</h3>
         </div>
 
         <div>
