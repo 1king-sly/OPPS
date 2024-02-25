2f1336bd4024ad01e53532e70ff253b131aaff82
Byrone Kinsly <Kinslybyrone17@gmail.com>
2024-02-25 19:25:50 +0300

    adding draft pages for pre-submitted projects

---
+++ app/(ui)/User/(pages)/Drafts/[id]/page.tsx
@@ -0,0 +1,112 @@
+'use server'
+import React from 'react';
+import { getServerSession } from 'next-auth';
+import {  fetchSingleProject } from '@/app/lib/actions';
+import NotFound from './not-found';
+import { redirect } from 'next/navigation';
+
+export default async function Page({ params }: { params: { id: string } }) {
+
+  const session = await getServerSession()
+  if(!session){
+    redirect('/')
+  }
+  
+    const projectId = params.id
+    const project = await fetchSingleProject(projectId)
+
+    if(!project){
+      return <NotFound/>
+    }
+    
+ 
+  return (
+    <>
+      <div className='w-full min-h-screen flex flex-col items-center justify-center pb-16 mt-4'>
+        <div className='flex flex-col items-center'>
+          <div className='w-5/6 mx-auto flex flex-col text-black gap-10'>
+            <div>
+            <h1 className='w-full flex justify-center md:text-lg gap-1'>Title: <span className=' underline'>{project?.title}</span>  </h1>
+            <div className='  px-4'>
+                <div className='w-full flex  justify-center '>Problem identification and background/Needs assessment</div>
+                <div className=' text-sm'>What issue/challenge/gap does the project aim to address? The objectives should be clear, measureable, realistic and achievable within the duration of the project. For each objective, define appropriate indicators for measuring achievement (including a unit of measurement, baseline value and target value)</div>
+            </div>
+            <div className='mt-2  px-4 bg-gray-100 py-2  '>
+              <p className=' text-md '>{project?.ans1} </p>
+            </div>
+            </div>
+            <div>
+            <div className='  px-4'>
+                <div className='w-full flex  justify-center '>Research Purpose and anticipated results</div>
+              
+            </div>
+            <div className='mt-2  px-4 bg-gray-100 py-2  '>
+              <p className=' text-md '>{project?.ans2} </p>
+            </div>
+            </div>
+            <div>
+            <div className='  px-4'>
+                <div className='w-full flex  justify-center '>Project Design and Methodology</div>
+                <div className=' text-sm'>Outline the approach and methodology behind the project. Explain why they are the most suitable for achieving the project’s objectives.</div>
+            </div>
+            <div className='mt-2  px-4 bg-gray-100 py-2 '>
+              <p className=' text-md '>{project?.ans3} </p>
+            </div>
+            </div>
+            <div>
+            <div className='  px-4'>
+                <div className='w-full flex  justify-center '> Gender Equality, Equity, and Inclusion considerations</div>
+            </div>
+            <div className='mt-2  px-4 bg-gray-100 py-2  '>
+              <p className=' text-md '>{project?.ans4} </p>
+            </div>
+
+            {project?.comment !== null || project?.comment=='' ?(
+              <>
+               <div className='  px-4 mt-4'>
+                <div className='w-full flex   font-semibold gap-1'>Reviewer Comment: <span
+                className=''>{project?.updatedBy} </span></div>
+            </div>
+            <div className='mt-2  px-4 bg-gray-100 py-2 '>
+              <p className=' text-md'>{project?.comment} </p>
+            </div>
+              </>
+            ):null}
+
+{project?.moderatorComment !== null || project?.moderatorComment=='' ?(
+              <>
+               <div className='  px-4 mt-4'>
+                <div className='w-full flex   font-semibold gap-1'>Moderator Comment: <span
+                className=''>{project?.moderatorName} </span></div>
+            </div>
+            <div className='mt-2  px-4 bg-gray-100 py-2 '>
+              <p className=' text-md'>{project?.moderatorComment} </p>
+            </div>
+              </>
+            ):null}
+
+            </div>
+
+
+          
+          <div>
+          
+          
+           
+
+
+          </div>
+       
+          
+          
+            
+
+          </div>
+
+         
+         
+        </div>
+       </div>
+    </>
+  );
+}
---
+++ app/(ui)/User/(pages)/Drafts/page.tsx
@@ -0,0 +1,119 @@
+'use server'
+import React from 'react';
+import { getServerSession } from 'next-auth';
+import { deleteSingleProject, fetchUser, fetchUserProjects } from '@/app/lib/actions';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
+import Search from '../../Component/Search';
+import { CheckCircleIcon, ClockIcon, ExclamationTriangleIcon,TrashIcon,EyeIcon } from '@heroicons/react/24/outline';
+import { authOptions } from '@/utils/authUptions';
+
+
+
+export default async function Tables({searchParams}:{searchParams:string}) {
+   const data =await getServerSession(authOptions)
+  const params = new URLSearchParams(searchParams);
+  const q = params.get('query') || '';
+
+  const datas = await fetchUserProjects(data?.id,q);
+
+  return (
+    <>
+ <div className=' p-10  pb-40 max-[425px]:p-2 '>
+
+ <Search placeholder="Search for a project ..."/>
+ <table className=' w-full'>
+        
+
+        <tbody className='  flex-col mt-4 gap-3 flex'>
+
+       
+
+          {datas?.map((data)=>(
+            
+
+            <tr className='min-[426px]:justify-around  flex bg-gray-100 py-2 justify-between 
+            w-full pr-2 items-center ' key={data.projectId}>
+                  <td className='max-[425px]:w-3/5 max-[375px]:w-4/6 max-[320px]:w-3/5 w-1/3 truncate'>{data.title}  </td>
+                  <td className='w-1/3 max-[425px]:hidden' >{data.createdAt.toLocaleDateString()} </td>
+                  <td className='w-1/12' >
+                      {data.status === 'PENDING' || data.status ==='REFERRED' ?(
+                        <>
+                        <div className='w-full flex gap-0.5 justify-center bg-gray-300 p-2 lg:rounded-md
+                        rounded-full '>
+                        <ClockIcon className='h-3 w-3 md:w-4 md:h-4 lg:hidden'/>
+
+                        <p className='hidden lg:block text-xs'>
+                          PENDING
+                          </p>
+                           </div>
+                        </>
+                      ): null}
+                      {data.status === 'ACCEPTED' ?(
+                        <>
+                        <div className='w-full flex gap-0.5 justify-center bg-green-300 p-2 lg:rounded-md
+                        rounded-full'>
+                          <CheckCircleIcon className='h-3 w-3 md:w-4 md:h-4 lg:hidden'/>
+                     
+                        <p className='text-xs hidden lg:block '>
+                          ACCEPTED</p>
+                           </div>
+                        </>
+                      ): null}
+                      {data.status === 'REJECTED' ?(
+                        <>
+                        <div className='w-full flex  bg-rose-500 p-2 justify-center lg:rounded-md
+                        rounded-full  '>
+                          <ExclamationTriangleIcon className='h-3 w-3 md:w-4 md:h-4 lg:hidden'/>
+
+                        <p className='text-xs hidden lg:block '>
+                          REJECTED</p>
+                      
+                           </div>
+                        </>
+                      ): null}
+                      </td>
+                  <Link href={`/User/Projects/${data.projectId}`} key={data.projectId}>
+                  <td className='w-1/12' >
+                  <button className='bg-sky-300 p-2 text-white text-sm lg:rounded-md rounded-full '>
+                    <div>
+                      <EyeIcon className=' h-3 w-3 md:w-4 md:h-4 lg:hidden'/>
+                      <p className='hidden lg:block text-xs'>
+                         View
+
+                      </p>
+                    </div>
+                    </button>
+                  </td>
+                  </Link>
+
+                  <td className='w-1/12' >
+                    {data.status === 'PENDING' ?(
+                  <>
+                  <form action={deleteSingleProject} className='bg-rose-500 p-2 text-white text-sm rounded-md w-full flex items-center justify-center'>
+                      <input type="text" hidden value={data.projectId} name='projectId' />
+                      <button>
+                      <div>
+                        <TrashIcon className='h-3 w-3 md:w-4 md:h-4 lg:hidden'/>
+                        <p className='hidden lg:block text-xs'>
+                        Delete
+                        </p>
+                      </div>
+                      
+                      </button>                    </form>
+                  
+                  </>
+                 ): null}
+                  
+                  </td>
+                </tr>
+
+          ))}
+   
+        </tbody>
+      </table>
+    </div>
+      
+    </>
+  );
+}
